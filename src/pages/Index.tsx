92d342416464e7e3494bf825af74d6a2f23f747e
lilJ <julia.s@live.nl>
2025-09-09 10:15:55 +0200

    setup

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,19 +1,9 @@
 import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Gallery from "../components/Gallery";
-import About from "../components/About";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
 
 const Index = () => {
   return (
     <main className="min-h-screen">
       <Header />
-      <Hero />
-      <Gallery />
-      <About />
-      <Contact />
-      <Footer />
     </main>
   );
 };
