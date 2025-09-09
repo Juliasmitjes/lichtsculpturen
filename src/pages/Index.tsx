e860bf41619d45fd72af5e446673251bdd59fcc4
lilJ <julia.s@live.nl>
2025-09-09 13:45:59 +0200

    add gallery page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Gallery from "../components/Gallery";
 
 const Index = () => {
   return (
     <main className="min-h-screen">
       <Header />
       <Hero />
+      <Gallery />
     </main>
   );
 };
