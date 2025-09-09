42e110a580b79f1fb0599d25e62b7b645d256468
lilJ <julia.s@live.nl>
2025-09-09 10:52:42 +0200

    import hero

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const Index = () => {
   return (
     <main className="min-h-screen">
       <Header />
+      <Hero />
     </main>
   );
 };
